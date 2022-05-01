173ebc260fbf4757a9dcf77f2869215c35eb1ae4
Dibyam <dibyam1101@gmail.com>
2022-05-01 16:48:13 +0530

    Added Restaurants near me in Home Page

--- food_ordering_app/lib/features/customer/home/presentation/widgets/nearby_restaurant_list.dart
+++ food_ordering_app/lib/features/customer/home/presentation/widgets/nearby_restaurant_list.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:food_ordering_app/features/customer/home/presentation/widgets/restaurant_card.dart';
 
 class NearbyRestaurantList extends StatelessWidget {
   const NearbyRestaurantList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Restaurants around you ',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              letterSpacing: 1.5,
+            ),
+          ),
+          ListView.builder(
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return RestaurantCard();
+              }),
+        ],
+      ),
+    );
   }
 }
--- food_ordering_app/lib/features/customer/home/presentation/widgets/restaurant_card.dart
+++ food_ordering_app/lib/features/customer/home/presentation/widgets/restaurant_card.dart
@@ -5,6 +5,52 @@ class RestaurantCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: ListTile(
+          contentPadding: EdgeInsets.all(0),
+          title: Image.asset(
+            'assets/images/profile.jpg',
+          ),
+          subtitle: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        'Red Rock Agency',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            letterSpacing: 1.5,
+                            color: Colors.black),
+                      ),
+                    ),
+                    Text('3.1 Stars')
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text('Pizza, Burger, Momos'),
+                    ),
+                    Text('Rs. 150 for one'),
+                  ],
+                ),
+                Text('We recycle more plastic than used in our orders'),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
